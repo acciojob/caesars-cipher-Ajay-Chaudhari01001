e3c1e8c97b9a90b902c5d7ce7e37297440149d05
Ajay Chaudhari <55138445+Ajay-Chaudhari01001@users.noreply.github.com>
2023-04-14 14:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-// Your Script here.
-
-const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
-
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return ;//return decodedArr
+function rot13(str) {
+  var decoded = "";
+  
+  for (var i = 0; i < str.length; i++) {
+    var char = str.charAt(i);
+    if (char.match(/[A-Z]/)) {
+      var code = char.charCodeAt(0);
+      if (code >= 65 && code <= 77) {
+        char = String.fromCharCode(code + 13);
+      } else {
+        char = String.fromCharCode(code - 13);
+      }
+    }
+    decoded += char;
+  }
+  
+  return decoded;
 }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-module.exports = rot13;
