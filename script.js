490a5d51d7481eacf5f49de0207942c12f03c95c
Ajay Chaudhari <55138445+Ajay-Chaudhari01001@users.noreply.github.com>
2023-04-14 14:43:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 function rot13(str) {
-  var decoded = "";
-  
-  for (var i = 0; i < str.length; i++) {
-    var char = str.charAt(i);
-    if (char.match(/[A-Z]/)) {
-      var code = char.charCodeAt(0);
-      if (code >= 65 && code <= 77) {
-        char = String.fromCharCode(code + 13);
-      } else {
-        char = String.fromCharCode(code - 13);
-      }
-    }
-    decoded += char;
-  }
-  
-  return decoded;
+var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+var index = x => input.indexOf(x);
+var translate = x => index(x) > -1 ? output[index(x)] : x;
+return str.split('').map(translate).join('');
 }
